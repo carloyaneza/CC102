3b3c92c0e8fdd6ab3018dd666665a9af2e7a3350
carloyaneza <carloyaneza406@gmail.com>
2024-10-07 09:27:42 +0800

    Add files via upload

---
+++ CC102-02BasicOutput.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "School: \t\t\t | Our Lady of Lourdes College Foundation" << endl;
+	cout << "Department: \t\t\t | Computer Studies" << endl;
+	cout << "Course & Year Level: \t\t\t | BS in Information Technology" << endl;
+	cout << "Full name \t\t\t | John Carlo B. Yaneza" << endl;
+	cout << "Address \t\t\t | P-5 Barangay Cobangbang Daet Camarines Norte" << endl;
+	
+	return 0;
+	
+}
