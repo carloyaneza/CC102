c1ca5f8d0d7f61e3523cf1baa4d342806e64b0b1
carloyaneza <carloyaneza406@gmail.com>
2024-10-07 09:45:54 +0800

    Add files via upload

---
+++ CC102-04Basicinput.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+int main()
+ 
+ {
+	 string nickname, cy1, school;
+	 
+	 cout << "Nickname: ";
+	 cin >> nickname;
+	 
+	 cout << "Course, year level: ";
+	 cin >> cy1;
+	 
+	 cout << "School: ";
+	 cin >> school;
+	 
+	 cout << "Wow congrats" <<nickname<< "!" <<cy1<< "is a nice course, and you are studying in" <<school<< "Which is one of the Center  of Excellence in Tertiary Education " << endl;
+	 
+	 return 0;
+ }
